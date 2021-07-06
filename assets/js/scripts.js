b8d7ce564487ef86d344ac98f838f013c2dc6698
Zelhorst92 <r.zelhorst@gmail.com>
2021-07-06 14:45:03 +0000

    Add get categories from API function

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,22 +1,44 @@
+/* ----------------- Getting any data from opentbd.com API */
+
+let baseURL = "https://opentdb.com/"
+
+let getData = function (url) {
+    return fetch(url).then(Response => Response.json());
+}
+
+/* ----------------- Getting category data from opentbd.com API */
+
+let selectCategory = document.getElementById("category-selector");
+let getCategories = getData(baseURL + "api_category.php");
+
+getCategories.then(function (data) {
+    categories = data.trivia_categories; /* array of categories taken from API*/
+    for(let category of categories) {
+        (selectCategory.options[selectCategory.options.length] = new Option(category.name, category.id)); /* Create an option list for the category selector */
+    };
+});
+
+/* ----------------- Getting question data from opentbd.com API */
+
 /* ----------------- Collapse animation Script */
 
-document.getElementById('start').addEventListener("click", function () {
+document.getElementById("start").addEventListener("click", function () {
     setTimeout(
         function () {
-            document.getElementById('game-circle').className += ' inner-circle-load';
-            document.getElementById('category-selector').className += ' hide';
-            document.getElementById('start').className += ' hide';
+            document.getElementById("game-circle").className += " inner-circle-load";
+            document.getElementById("category-container").className += " hide";
+            document.getElementById("start").className += " hide";
         }, 500
     );
     setTimeout(
         function () {
-            document.getElementById('game-circle').className = 'inner-circle';
+            document.getElementById("game-circle").className = "inner-circle";
         }, 4000
     );
     setTimeout(
         function () {
-            document.getElementById('category-selector').className = '';
-            document.getElementById('start').className = 'btn btn-start';
+            document.getElementById("category-container").className = "";
+            document.getElementById("start").className = "btn btn-start";
         }, 4500
     );
 });
