d650e43bbb5f3ae7502da1b110a85176af889200
Zelhorst92 <r.zelhorst@gmail.com>
2021-07-07 15:50:39 +0000

    Fix category select for questions fetch

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -16,10 +16,18 @@ getCategories.then(function (result) {
 
 /* ----------------- Getting question data from opentbd.com API with selected category*/
 
-document.getElementById("category-selector").addEventListener("click", fetchQuestions());
+
+document.getElementById("category-selector").addEventListener("click", function () {
+    fetchQuestions()
+});
+
+document.getElementById("start").addEventListener("click", function () {
+    fetchQuestions()
+});
 
 function fetchQuestions() {
     let categoryId = document.getElementById("category-selector").value;
+
     const fetchedQuestions = fetch(`https://opentdb.com/api.php?amount=20&category=${categoryId}&type=multiple`)
         .then(Response => Response.json())
         .catch(err => {
@@ -42,6 +50,8 @@ function fetchQuestions() {
     });
 };
 
+
+
 /* ----------------- */
 
 const question = document.getElementById("question");
@@ -60,141 +70,147 @@ const correctPoint = 1;
 const maxQuestions = 20;
 
 document.getElementById("start").addEventListener("click", function startGame() {
+            if (questions == 0) {
+                fetchedQuestions();
+            } else {
 
-    questionCounter = 0;
-    score = 0;
-    availableQuestions = [...questions];
-    /* console.log(availableQuestions); */
-    getNewQuestion();
-    setTimeout(
-        function () {
-            document.getElementById("game-circle").className += " inner-circle-load";
-            document.getElementById("category-container").className += " hide";
-            document.getElementById("start").className += " hide";
-            document.getElementById("linkHelpModal").className += " hide"
-            document.getElementById("restart").className = "give-up";
-
-        }, 200
-    );
-    setTimeout(
-        function () {
-            /* loadQuestions */
-            document.getElementById("game-circle").className = "inner-circle";
-            document.getElementById("tally-container").className = "";
-            document.getElementById("question").className = "";
-            for (let choice of choices) {
-                choice.className = "btn answer-choice"
-            }
-        }, 3000
-    );
-    setTimeout(
-        function () {
-            document.getElementById("left").className = "timer left"
-            document.getElementById("right").className = "timer right"
-        }, 4000
-    );
-})
-
-/* ----------------- Get a new question */
-
-function getNewQuestion() {
-
-    if (availableQuestions.length === 0) {
-        question.innerText = "Game Done"; /* Temporary end message */
-        for (let choice of choices) {
-            choice.className = "btn answer-choice hide"
-        };
-    } else {
-        questionCounter++;
-        questionCounterProgress.innerText = questionCounter+"/"+maxQuestions;
-
-        const questionIndex = Math.floor(Math.random() * availableQuestions.length);
-        currentQuestion = availableQuestions[questionIndex];
-        question.innerText = currentQuestion.question;
-
-        for (let choice of choices) {
-            const number = choice.dataset["number"];
-            choice.innerText = currentQuestion["choice" + number];
-        };
-
-        availableQuestions.splice(questionIndex, 1);
-
-        acceptingAnswers = true;
-    };
-};
-
-/* ----------------- Check answer */
-
-for (let choice of choices) {
-    choice.addEventListener("click", function (event) {
-        if (!acceptingAnswers) return;
-
-        acceptingAnswers = false;
-        const selectedChoice = event.target;
-        const selectedAnswer = selectedChoice.dataset["number"];
-
-        let classToApply = "incorrect"
-            if(selectedAnswer == currentQuestion.answer) {
-                classToApply = "correct";
-            };
-
-            if(classToApply === "correct") {
-                addScore(correctPoint);
-            }
-
-        selectedChoice.classList.add(classToApply);
-        document.getElementById("outer-circle").className = classToApply
-        
-        setTimeout(
-            function() {
-                selectedChoice.classList.remove(classToApply);
-                document.getElementById("outer-circle").className = "neutral";
-                document.getElementById("left").className = "timer left hide"
-                document.getElementById("right").className = "timer right hide"
-
+                questionCounter = 0;
+                score = 0;
+                availableQuestions = [...questions];
+                console.log(availableQuestions);
                 getNewQuestion();
-            }, 1000
-        );
-        setTimeout(
-            function() {
-                document.getElementById("left").className = "timer left"
-                document.getElementById("right").className = "timer right"
-            }, 1100
-        );
-        console.log(classToApply);
-    });
-};
+                setTimeout(
+                    function () {
+                        document.getElementById("game-circle").className += " inner-circle-load";
+                        document.getElementById("category-container").className += " hide";
+                        document.getElementById("start").className += " hide";
+                        document.getElementById("linkHelpModal").className += " hide"
+                        document.getElementById("restart").className = "give-up";
 
-function addScore (num) {
-    score += num;
-    scoreProgress.innerText = score;
-}
-
-
-/* ----------------- Modal Help Script */
-
-let modal = document.getElementById("helpModal");
-let help = document.getElementById("linkHelpModal");
-let close = document.getElementById("helpExit")
-
-help.onclick = function () {
-    modal.style.display = "block";
-}
-
-close.onclick = function () {
-    modal.style.display = "none";
-}
-
-window.onclick = function (event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
-
-/* ----------------- Restart Script */
-
-document.getElementById("restart").addEventListener("click", function () {
-    if (!confirm("Are you sure you want to quit the game and go back to the menu?")) {} else {
-        window.location.reload();
-    }
+                    }, 200
+                );
+                setTimeout(
+                    function () {
+                        /* loadQuestions */
+                        document.getElementById("game-circle").className = "inner-circle";
+                        document.getElementById("tally-container").className = "";
+                        document.getElementById("question").className = "";
+                        for (let choice of choices) {
+                            choice.className = "btn answer-choice"
+                        }
+                    }, 3000
+                );
+                setTimeout(
+                    function () {
+                        document.getElementById("left").className = "timer left"
+                        document.getElementById("right").className = "timer right"
+                    }, 4000
+                );
+            };
 });
+    
+
+
+        /* ----------------- Get a new question */
+
+        function getNewQuestion() {
+
+            if (availableQuestions.length === 0) {
+                question.innerText = "Game Done"; /* Temporary end message */
+                for (let choice of choices) {
+                    choice.className = "btn answer-choice hide"
+                };
+            } else {
+                questionCounter++;
+                questionCounterProgress.innerText = questionCounter + "/" + maxQuestions;
+
+                const questionIndex = Math.floor(Math.random() * availableQuestions.length);
+                currentQuestion = availableQuestions[questionIndex];
+                question.innerText = currentQuestion.question;
+
+                for (let choice of choices) {
+                    const number = choice.dataset["number"];
+                    choice.innerText = currentQuestion["choice" + number];
+                };
+
+                availableQuestions.splice(questionIndex, 1);
+
+                acceptingAnswers = true;
+            };
+        };
+
+        /* ----------------- Check answer on click */
+
+        for (let choice of choices) {
+            choice.addEventListener("click", function (event) {
+                if (!acceptingAnswers) return;
+
+                acceptingAnswers = false;
+                const selectedChoice = event.target;
+                const selectedAnswer = selectedChoice.dataset["number"];
+
+                let classToApply = "incorrect"
+                if (selectedAnswer == currentQuestion.answer) {
+                    classToApply = "correct";
+                };
+
+                if (classToApply === "correct") {
+                    addScore(correctPoint);
+                }
+
+                selectedChoice.classList.add(classToApply);
+                document.getElementById("outer-circle").className = classToApply
+
+                setTimeout(
+                    function () {
+                        selectedChoice.classList.remove(classToApply);
+                        document.getElementById("outer-circle").className = "neutral";
+                        document.getElementById("left").className = "timer left hide"
+                        document.getElementById("right").className = "timer right hide"
+
+                        getNewQuestion();
+                    }, 1000
+                );
+                setTimeout(
+                    function () {
+                        document.getElementById("left").className = "timer left"
+                        document.getElementById("right").className = "timer right"
+                    }, 1100
+                );
+                console.log(classToApply);
+            });
+        };
+
+        function addScore(num) {
+            score += num;
+            scoreProgress.innerText = score;
+        }
+
+
+        /* ----------------- Modal Help Script */
+
+        let modal = document.getElementById("helpModal");
+        let help = document.getElementById("linkHelpModal");
+        let close = document.getElementById("helpExit")
+
+        help.onclick = function () {
+            modal.style.display = "block";
+        }
+
+        close.onclick = function () {
+            modal.style.display = "none";
+        }
+
+        window.onclick = function (event) {
+            if (event.target == modal) {
+                modal.style.display = "none";
+            }
+        }
+
+        /* ----------------- Restart Script */
+
+        document.getElementById("restart").addEventListener("click", function () {
+            if (!confirm("Are you sure you want to quit the game and go back to the menu?")) {} else {
+                window.location.reload();
+            }
+        });
