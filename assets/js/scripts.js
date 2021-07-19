e199ab74efa42849b0a10f6a18ae866382339305
Zelhorst92 <r.zelhorst@gmail.com>
2021-07-19 17:01:03 +0000

    Fix issue array not being filled with 20 questions

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -54,7 +54,7 @@ const createSelectBox = (categories) => {
 // ----------------- Start the game
 
 startBtnRef.addEventListener("click", startGame = () => {
-    if (questions.length === 0) {
+    if (questions.length !== 20) {
         console.log("Number of questions in the array: " + questions.length);
         console.log("Questions array is empty, fetching questions...");
         fetchQuestions();
